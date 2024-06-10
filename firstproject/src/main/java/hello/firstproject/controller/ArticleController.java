973b49974b74d9d3c61b28118bb057ef6d656380
KAH <kahback99@gmail.com>
2024-06-11 00:05:19 +0900

    5 chapter

--- firstproject/src/main/java/hello/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/hello/firstproject/controller/ArticleController.java
@@ -6,8 +6,13 @@ import hello.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -29,6 +34,27 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info("DB에 저장된 Article 엔티티 : {}",saved);
         return "articles/new";
+    }
+
+    @GetMapping("/article/{id}") //데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model){
+        log.info("id : {}",id);
+        // id를 조회해 데이터를 가져오기
+        Optional<Article> articleEntity = articleRepository.findById(id);
+        //모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity.orElse(null));
+        // 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. DB에서 모든 Article 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2. 가져온 Article 묶음을 모델로 등록하기
+        model.addAttribute("articleList",articleEntityList);
+        // 3. 사용자에게 보여줄 뷰 페이지 설정하기
+        return "articles/index";
     }
 
 }
--- firstproject/src/main/java/hello/firstproject/repository/ArticleRepository.java
+++ firstproject/src/main/java/hello/firstproject/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package hello.firstproject.repository;
 import hello.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
