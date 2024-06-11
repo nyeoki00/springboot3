9f624679bd34bf270195b86009742a09918e27d5
KAH <kahback99@gmail.com>
2024-06-11 23:12:22 +0900

    8 chapter

--- firstproject/src/main/java/hello/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/hello/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -78,6 +79,22 @@ public class ArticleController {
         }
         // 3. 수정 결과 페이지로 리다이렉트하기
         return "redirect:/articles/"+articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다");
+        // 1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        // 2. 대상 엔티티 삭제하기
+        if(target != null){
+            articleRepository.delete(target);
+        }
+        rttr.addFlashAttribute("msg","삭제됐습니다!");
+        // 3. 결과페이지로 리다이렉트하기
+
+        return "redirect:/articles";
     }
 
 }
